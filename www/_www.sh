8158604b4607bdf71a488cee2fff294a36bb83e0
Jason K Lai <jason.lai@bcm.edu>
2016-01-11 10:34:54 -0600

    Template for new www server configuration
    Added in a new folder for web servers. Currently calling it /www/, but may
    change to something like /web/ later since /www/ directories have other
    connotations. The directory is mostly blank for now.

---
+++ www/_www.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Script to reset the config files in the home directory. Checks if the file
+# exists and removes it before creating symbolic links from the current
+# directory to the appropriate path.
+
+resetConfig() {
+	# handle input arguments
+	config="$1"
+	link="$2"
+	dirname=$(dirname -- "$link")
+
+	echo "    resetting $config to $link"
+
+	# remove file if it exists
+	if [ -f "$link" ] || [ -d "$link" ]
+	then
+		rm -Rf -- "$link"
+	fi
+
+	# make the preceding directory if it does not exist
+	if [ ! -d "$dirname" ]
+	then
+		mkdir -p -- "$dirname"
+	fi
+
+	# link the remote config to the local link
+	ln -s "$config" "$link"
+}
+
+read -p "WARNING: Completely resets config for home directory [CTRL-C to quit] "
+
+# bash
+resetConfig $(pwd)/bash/_bash_logout ~/.bash_logout
+resetConfig $(pwd)/bash/_bash_profile ~/.bash_profile
+resetConfig $(pwd)/bash/_bashrc ~/.bashrc
+resetConfig $(pwd)/bash/_gitconfig ~/.gitconfig
+resetConfig $(pwd)/bash/_inputrc ~/.inputrc
+
