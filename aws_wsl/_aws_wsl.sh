c4a27a6547e4fa653858d7e8ad29d763d58b681e
Ubuntu <ubuntu@ip-172-31-92-187.ec2.internal>
2022-07-26 14:25:46 +0000

    Included a new set of configs (AWS on WSL)
    There are slight differences when considering the AWS config files on
    a WSL setup. A new directory for aws_wsl has been created here.

---
+++ aws_wsl/_aws_wsl.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Script to reset the config files in the home directory. Checks if the file
+# exists and removes it before creating symbolic links from the current
+# directory to the appropriate path.
+
+resetConfig() {
+	# handle input arguments
+	config="$1"
+	link="$2"
+	dirname=$(dirname -- "$link")
+
+	echo "    resetting $config to $link"
+
+	# remove file if it exists
+	if [ -f "$link" ] || [ -d "$link" ]
+	then
+		rm -Rf -- "$link"
+	fi
+
+	# make the preceding directory if it does not exist
+	if [ ! -d "$dirname" ]
+	then
+		mkdir -p -- "$dirname"
+	fi
+
+	# link the remote config to the local link
+	ln -s "$config" "$link"
+}
+
+read -p "WARNING: Completely resets config for home directory [CTRL-C to quit] "
+
+# bash
+resetConfig $(pwd)/bash/_bashrc ~/.bashrc
+resetConfig $(pwd)/bash/_inputrc ~/.inputrc
+resetConfig $(pwd)/bash/_minttyrc ~/.minttyrc
+
+# elinks
+resetConfig $(pwd)/elinks/_bookmarks.html ~/.bookmarks.html
+resetConfig $(pwd)/elinks/_elinks/elinks.conf ~/.elinks/elinks.conf
+resetConfig $(pwd)/elinks/_elinks/newsbeuter.conf ~/.elinks/newsbeuter.conf
+
+# newsbeuter
+resetConfig $(pwd)/newsbeuter/_newsbeuter/config ~/.newsbeuter/config
+resetConfig $(pwd)/newsbeuter/_newsbeuter/urls ~/.newsbeuter/urls
+
+# rtv
+resetConfig $(pwd)/rtv/rtv.cfg ~/.config/rtv/rtv.cfg
+
+# screen
+resetConfig $(pwd)/screen/_screenrc ~/.screenrc
+
+# vifm
+resetConfig $(pwd)/vifm/_vifm/colors ~/.vifm/colors
+resetConfig $(pwd)/vifm/_vifm/vifmrc ~/.vifm/vifmrc
+
+# vim
+resetConfig $(pwd)/vim/_vimrc ~/.vimrc
+resetConfig $(pwd)/vim/_vim/doc ~/.vim/doc
+resetConfig $(pwd)/vim/_vim/plugin ~/.vim/plugin
+
